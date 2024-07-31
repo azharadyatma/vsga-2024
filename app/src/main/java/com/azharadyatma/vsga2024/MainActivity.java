4160610a495189681b287e2a05867b5803bdc527
azharadyatma <2210631170011@student.unsika.ac.id>
2024-07-31 07:11:27 +0700

    Membuat name store SQLite

--- app/src/main/java/com/azharadyatma/vsga2024/MainActivity.java
+++ app/src/main/java/com/azharadyatma/vsga2024/MainActivity.java
@@ -1,24 +1,50 @@
 package com.azharadyatma.vsga2024;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnStore, btnGetall;
+    private EditText etname;
+    private DatabaseHelper databaseHelper;
+    private TextView tvnames;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        databaseHelper = new DatabaseHelper(this);
+        tvnames = findViewById(R.id.tvnames);
+        btnStore = findViewById(R.id.btnstore);
+        btnGetall = findViewById(R.id.btnget);
+        etname = findViewById(R.id.etname);
+
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseHelper.addStudentDetail(etname.getText().toString());
+                etname.setText("");
+                Toast.makeText(MainActivity.this, "Stored Successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnGetall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList = databaseHelper.getAllStudentsList();
+                tvnames.setText("");
+                for (int i = 0; i < arrayList.size(); i++) {
+                    tvnames.append(arrayList.get(i) + ", ");
+                }
+            }
         });
     }
 }
